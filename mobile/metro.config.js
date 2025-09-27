c953437f1782cf16a975cf474a4264a88bb2b877
Bin Sheng <supercarl87@gmail.com>
2025-09-27 14:23:18 -0700

    npx start working

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Ensure that the app directory is properly resolved for expo-router
-process.env.EXPO_ROUTER_APP_ROOT = './app';
-
 module.exports = config;
